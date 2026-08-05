6390fbc06b4d56fbcb21d53cdb2e89470cc3fd17
= <hb24mcf1r14@student.nitw.ac.in>
2026-08-06 00:24:22 +0530

    Update main.go example output

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
-
 import "fmt"
-
+//format package
 func main() {
 	fmt.Println("Hello Harsh!")
-	fmt.Scanln("hey bob ! Enter ur name")
+	fmt.Scanln()
+	var x = 6 
+	var y int = 5
+	z := 4
+	fmt.Println(x,"  ",y,z)
 }
+
+
+//if you create a var , using it is important
